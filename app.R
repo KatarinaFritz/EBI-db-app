76e27dc2ba6b075351f67ec7e4db18965f1bec02
kjemist <illimar.rekand@uib.no>
2023-06-20 16:00:23 +0200

    Added bar plot which describes the institutional affiliation of submitted datasets

--- app.R
+++ app.R
@@ -2,17 +2,21 @@
 # Plot them in ggplot
 # Shiny
 # June 2023, Illimar Rekand (illimar.rekand@uib.no)
+# ELIXIR Bergen, UiB
+# To run the Shiny app, use runApp("EBI-db-app-local") in the console in dir where the EBI-db-app-local folder is located
 
 library(httr)
 library(jsonlite)
 library(ggplot2)
 library(wesanderson) #colorpalette
 library(shiny)
+library(tidyverse)
+library(dplyr)
 
 
 #size limits the search to the first n hits. Otherwise, only 15 first are included
 # URL for the fields available in PRIDE: https://www.ebi.ac.uk/ebisearch/metadata.ebi?db=pride
-query = "https://www.ebi.ac.uk/ebisearch/ws/rest/pride?query=submitter_country:Norway&size=1000&fields=submission_date,publication_date&format=json"
+query = "https://www.ebi.ac.uk/ebisearch/ws/rest/pride?query=submitter_country:Norway&size=1000&fields=submission_date,publication_date,labhead_affiliation,submitter_affiliation,labhead_mail,labhead,search_count,view_count,download_count,citation_count,omics_type,species&format=json"
 
 #SimplifyDataFrame = T enables data retrieved into a dataframe format
 # Sometimes the command below crashes. giving a CacheKey error. If this happens, restart R session (ctrl+shift+F10)
@@ -25,7 +29,6 @@ typeof(datasets[[2]])
 dim(datasets[[2]])
 
 datasets.df <- as.data.frame(datasets[[2]])
-datasets.df[1,3]
 #convert to character first, because conversion directly to date does not work....
 datasets.df$fields.submission_date <- as.character(datasets.df$fields.submission_date)
 datasets.df$fields.submission_date <- format(as.Date(datasets.df$fields.submission_date, format = "%Y%m%d"), "%Y")
@@ -33,8 +36,45 @@ datasets.df$fields.submission_date <- format(as.Date(datasets.df$fields.submissi
 datasets.df$fields.publication_date <- as.character(datasets.df$fields.publication_date)
 datasets.df$fields.publication_date <- format(as.Date(datasets.df$fields.publication_date, format = "%Y%m%d"), "%Y")
 
-#datasets.df$fields.publication_date <- DateViaChar(datasets.df$fields.publication_date)
-#typeof(datasets.df$fields.publication_date)
+datasets.df$fields.labhead_affiliation <- as.character(datasets.df$fields.labhead_affiliation)
+datasets.df$fields.labhead_mail  <- as.character(datasets.df$fields.labhead_mail)
+
+
+#datasets.df$fields.labhead_affiliation
+#datasets.df %>% 
+  #mutate(affiliation_shortened = if(grepl("Bergen", datasets.df$fields.labhead_affiliation)) "UiB") %>%
+  #fill(affiliation_shortened) %>%
+  #as.data.frame()
+
+#lapply(datasets.df$fields.labhead_affiliation, function(x) if(identical(x, character(0))) "missing" else x)
+
+datasets.df$fields.labhead_affiliation
+
+datasets.df$affiliation_shortened <- ifelse(grepl("Science and Technology", datasets.df$fields.labhead_affiliation, ignore.case =  T), "NTNU", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("Bergen", datasets.df$fields.labhead_affiliation, ignore.case = T)|grepl("PROBE", datasets.df$fields.labhead_affiliation)|grepl("UiB", datasets.df$fields.labhead_affiliation), "UiB", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("uib", datasets.df$fields.labhead_mail, ignore.case =  T), "UiB", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("Oslo", datasets.df$fields.labhead_affiliation), "UiO", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("uio", datasets.df$fields.labhead_mail), "UiO", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("UiT", datasets.df$fields.labhead_affiliation, ignore.case = T)|grepl("Troms", datasets.df$fields.labhead_affiliation, ignore.case = T), "UiT", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("uit", datasets.df$fields.labhead_mail), "UiT", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("Life Sciences", datasets.df$fields.labhead_affiliation, ignore.case = T)|grepl("NMBU", datasets.df$fields.labhead_affiliation, ignore.case = T), "NMBU", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("Nord university", datasets.df$fields.labhead_affiliation, ignore.case =  T), "NORD", datasets.df$affiliation_shortened)
+datasets.df$affiliation_shortened <- ifelse(grepl("UiS", datasets.df$fields.labhead_affiliation, ignore.case =  T)|grepl("Stavanger", datasets.df$fields.labhead_affiliation, ignore.case =  T), "UiS", datasets.df$affiliation_shortened)
+
+
+##dataframes below are for debugging
+
+#debug.df <- datasets.df[, c("id",
+                            ##"fields.labhead_mail",
+                            #"affiliation_shortened", "fields.labhead_affiliation")]
+#debug.df
+
+#write.table(debug.df, file = "debug-df.txt", sep = "\t")
+
+
+#datasets.df$affiliation_shortened <- lapply(datasets.df$affiliation_shortened, function(x) if(grepl("Bergen", datasets.df$fields.labhead_affiliation)) "UiB" else x)
+datasets.df$fields.labhead_affiliation
+
 
 pal <- wes_palette("GrandBudapest2", 11, type = "continuous")
 
@@ -46,12 +86,16 @@ pal <- wes_palette("GrandBudapest2", 11, type = "continuous")
 #  theme(legend.position = "none")
 #test_plot
 
-#test_plot_2 <- ggplot(datasets.df, aes(x= fields.publication_date, fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
-#  geom_bar() + 
-#  theme_classic() + 
-#  scale_fill_manual(values = pal) +
-#  theme(legend.position = "none")
-#test_plot_2
+reorder <- TRUE
+test_plot_2 <- ggplot(datasets.df, aes(
+  if(reorder == TRUE){ x= fct_rev(fct_infreq(affiliation_shortened)) }
+  else { x= affiliation_shortened },
+  fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
+  geom_bar() + 
+  theme_classic() + 
+  scale_fill_manual(values = pal) +
+  theme(legend.position = "none")
+test_plot_2
 
 #ggplot(datasets.df, aes(x= fields.publication_date)) + geom_bar() + theme_classic()
 
@@ -72,7 +116,9 @@ ui <- fluidPage(
       # The below variables will be output as character strings
       selectInput("variable", "Variable:",
                   c("Submission date" = "fields.submission_date", 
-                    "Publication date" = "fields.publication_date")),
+                    "Publication date" = "fields.publication_date",
+                    "Affiliation" = "affiliation_shortened"
+                    )),
       
       
     ),
@@ -107,8 +153,13 @@ server <- function(input, output) {
   
   # Generate a plot of the requested variable against count ----
   output$ebi.plot <- renderPlot({
-    ggplot(datasets.df, aes_string(x = input$variable))+ #aes_string is necessary because input$variable is a string
-      geom_bar(aes(fill = as.factor(after_stat(count)))) + #fill-component in aes needs to be declared here, because it is not compatible with aes_string
+    order_bars <- FALSE #we want institutions to be ordered after frequency, but not the years of publications
+    if(input$variable == "affiliation_shortened"){order_bars <- TRUE}
+    ggplot(datasets.df, aes(
+      if(order_bars == TRUE) {x = fct_rev(fct_infreq(!!sym(input$variable)))}
+      else {x = !!sym(input$variable)}
+      ))+ #input$variable is a string, !!sym() converts them into symbols
+      geom_bar(aes(fill = as.factor(after_stat(count)))) + #fill-component in aes needs to be declared here, because it is not compatible with aes_string ##this is probably not necessary after all with the impl of !!sym(), but we will keep it to make it easier to read
       theme_classic() + #remove gridlines
       scale_fill_manual(values = pal) + #enables the custom palette from wes_palette
       theme(legend.position = "none") # No legend
