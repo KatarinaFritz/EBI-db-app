855528c78489841847cec56f15c9e0766b66fa58
kjemist <illimar.rekand@uib.no>
2023-06-29 16:52:07 +0200

    added some more plots (species, diseases). Also added some heatplots testing plots, not built into shiny

--- EBI-db-app.R
+++ EBI-db-app.R
@@ -38,11 +38,80 @@ datasets.df$fields.publication_date <- format(as.Date(datasets.df$fields.publica
 
 pal <- wes_palette("Zissou1", 11, type = "continuous")
 
-ggplot(datasets.df, aes(x= fields.submission_date, fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
-  geom_bar() + 
-  theme_classic() + 
-  scale_fill_manual(values = pal) +
-  theme(legend.position = "none")
-ggplot(datasets.df, aes(x= fields.publication_date)) + geom_bar() + theme_classic()
+#ggplot(datasets.df, aes(x= fields.submission_date, fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
+#  geom_bar() + 
+#  theme_classic() + 
+#  scale_fill_manual(values = pal) +
+#  theme(legend.position = "none")
+
+#ggplot(datasets.df, aes(x= fields.publication_date, fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
+  #geom_bar() + 
+  #theme_classic() + 
+  #scale_fill_manual(values = pal) +
+  #theme(legend.position = "none")
+
+#ggplot(datasets.df, aes(x= fields.publication_date)) + geom_bar() + theme_classic()
 
 
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Number of datasets per year"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for variable to plot against mpg ----
+      selectInput("variable", "Variable:",
+                  c("Submission date" = "fields.submission_date",
+                    "Publication date" = "fields.publication_date")),
+      
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Formatted text for caption ----
+      h3(textOutput("caption")),
+      
+      # Output: Plot of the requested variable against count ----
+      plotOutput("ebi-plot")
+      
+    )
+  )
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+  # Compute the formula text ----
+  # This is in a reactive expression since it is shared by the
+  # output$caption and output$mpgPlot functions
+  formulaText <- reactive({
+    paste("after_stat(count) ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption ----
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg ----
+  # and only exclude outliers if requested
+  output$mpgPlot <- renderPlot({
+    ggplot(datasets.df, aes(x= variable, fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
+        geom_bar() + 
+        theme_classic() + 
+        scale_fill_manual(values = pal) +
+        theme(legend.position = "none"
+    )
+  })
+  
+  # Create Shiny app ----
+  shinyApp(ui, server)
+}
