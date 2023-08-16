c0c1a0f2e3d045f3266e288087d2f613bd22ff0b
kjemist <illimar.rekand@uib.no>
2023-08-16 15:43:19 +0200

    1. Fixed unpacking of lists inside columns 2. changed colors 3. added columns for dropdown-menu

--- app.R
+++ app.R
@@ -5,16 +5,25 @@
 # ELIXIR Bergen, UiB
 # To run the Shiny app, use runApp("EBI-db-app-local") in the console in dir where the EBI-db-app-local folder is located
 
-library(httr)
+#library(httr)
 library(jsonlite)
+library(dplyr)
 library(ggplot2)
-library(wesanderson) #colorpalette
 library(shiny)
 library(tidyverse)
-library(dplyr)
+
 library(ggwordcloud)
 library(wordcloud)
 
+find.count <- function(df, column){ #this functions creates a ggplot object and extracts the counts which are normally plotte don the y-variable
+  plot <- ggplot(df, aes(x = column)) + geom_bar()
+  plot.layer <- layer_data(plot, i = 1L) #extract info from the ggplot object
+  plot.count <- plot.layer$count #gives the count from the ggplot layer
+  return(plot.count)
+}
+
+#counts <- find.count(datasets.df, "fields.disease")
+#counts
 
 #size limits the search to the first n hits. Otherwise, only 15 first are included
 # URL for the fields available in PRIDE: https://www.ebi.ac.uk/ebisearch/metadata.ebi?db=pride
@@ -23,22 +32,10 @@ query = "https://www.ebi.ac.uk/ebisearch/ws/rest/pride?query=submitter_country:N
 #SimplifyDataFrame = T enables data retrieved into a dataframe format
 # Sometimes the command below crashes. giving a CacheKey error. If this happens, restart R session (ctrl+shift+F10)
 datasets.raw <- fromJSON(query, flatten = TRUE, simplifyDataFrame = TRUE)
-datasets.raw
 
-datasets.unpackt <- as_tibble(datasets.raw$entries) %>% 
-  unnest(fields.species) %>%
-  unnest(fields.tissue) %>%
-  unnest(fields.technology_type) %>%
-  unnest(fields.instrument_platform) %>%
-  unnest(fields.disease) %>%
-  unnest(fields.submitter_keywords)
-
-datasets.df <- do.call(rbind, datasets.raw) #convert to dataframe
+#datasets.df <- do.call(rbind, datasets.raw) #convert to dataframe
+datasets.df <- datasets.raw$entries
 datasets.df
-
-dataset.df.subset <- subset(datasets.df, select = c(id, fields.species))
-dataset.df.subset
-
 
 
 class(datasets.df)
@@ -48,16 +45,14 @@ dim(datasets.df)
 #convert to character first, because conversion directly to date does not work....
 datasets.df$fields.submission_date <- as.character(datasets.df$fields.submission_date)
 datasets.df$fields.submission_date <- format(as.Date(datasets.df$fields.submission_date, format = "%Y%m%d"), "%Y")
-datasets.df$fields.submission_date
 
 datasets.df$fields.publication_date <- as.character(datasets.df$fields.publication_date)
 datasets.df$fields.publication_date <- format(as.Date(datasets.df$fields.publication_date, format = "%Y%m%d"), "%Y")
-datasets.df$fields.publication_date 
 
-datasets.df$fields.labhead_affiliation <- as.character(datasets.df$fields.labhead_affiliation)
-datasets.df$fields.labhead_mail  <- as.character(datasets.df$fields.labhead_mail)
-datasets.df$fields.species  <- as.character(datasets.df$fields.species)
-datasets.df$fields.disease  <- as.character(datasets.df$fields.disease)
+#datasets.df$fields.labhead_affiliation <- as.character(datasets.df$fields.labhead_affiliation)
+#datasets.df$fields.labhead_mail  <- as.character(datasets.df$fields.labhead_mail)
+#datasets.df$fields.species  <- as.character(datasets.df$fields.species)
+#datasets.df$fields.disease  <- as.character(datasets.df$fields.disease)
 
 #datasets.df$fields.labhead_affiliation
 #datasets.df %>% 
@@ -71,6 +66,7 @@ datasets.df$affiliation_shortened <- "other" # All institutiions which do not be
                                              # Institution names are written in free-form, so the below text mining is necessary to group the affiliations properly
                                              # Below we are mining both the affiliation and the e-mail fields
 
+
 datasets.df$affiliation_shortened <- ifelse(grepl("Science and Technology", as.character(datasets.df$fields.labhead_affiliation), ignore.case =  T), "NTNU", datasets.df$affiliation_shortened)
 datasets.df$affiliation_shortened <- ifelse(grepl("Bergen", datasets.df$fields.labhead_affiliation, ignore.case = T)|grepl("PROBE", datasets.df$fields.labhead_affiliation)|grepl("UiB", datasets.df$fields.labhead_affiliation), "UiB", datasets.df$affiliation_shortened)
 datasets.df$affiliation_shortened <- ifelse(grepl("uib", datasets.df$fields.labhead_mail, ignore.case =  T), "UiB", datasets.df$affiliation_shortened)
@@ -83,54 +79,52 @@ datasets.df$affiliation_shortened <- ifelse(grepl("Life Sciences", datasets.df$f
 datasets.df$affiliation_shortened <- ifelse(grepl("Nord university", datasets.df$fields.labhead_affiliation, ignore.case =  T), "NORD", datasets.df$affiliation_shortened)
 datasets.df$affiliation_shortened <- ifelse(grepl("UiS", datasets.df$fields.labhead_affiliation, ignore.case =  T)|grepl("Stavanger", datasets.df$fields.labhead_affiliation, ignore.case =  T), "UiS", datasets.df$affiliation_shortened)
 
-datasets.df$affiliation_shortened
-pal <- wes_palette("Zissou1",11, type = "continuous")
-pal
 
 
 ##dataframes below are for debugging
 
 
-
 #filtered <- datasets.df %>% group_by(fields.species) %>% filter(n() > 5)
-x_value = "fields.publication_date"
-test_plot <- ggplot(datasets.df, aes(!!sym(x_value)))+ #as.factor harmonizes the color-palette with fill function of the graph
-  geom_bar(aes()) + 
-  theme_classic() + 
- scale_fill_manual(values = pal) +
-  #coord_flip() +
-  theme(legend.position = "none")
-test_plot
-## Below are some ggplots for debugging purposes
+#x_value = "fields.publication_date"
+#test_plot <- ggplot(datasets.df, aes(!!sym(x_value)))+ #as.factor harmonizes the color-palette with fill function of the graph
+  #geom_bar(aes()) + 
+  #theme_classic() + 
+ #scale_fill_manual(values = pal) +
+  ##coord_flip() +
+  #theme(legend.position = "none")
+#test_plot
+### Below are some ggplots for debugging purposes
 
-reorder <- TRUE
-x_value = "affiliation_shortened"
-test_plot_2 <- ggplot(datasets.unpackt, aes(
-  if(reorder == TRUE){ x= fct_rev(fct_infreq(!!sym(x_value))) }
-  else { x= !!sym("affiliation_shortened") },
-  fill = as.factor(after_stat(count))))+ #as.factor harmonizes the color-palette with fill function of the graph
-  geom_bar() + 
-  theme_classic() + 
-  scale_fill_manual(values = pal) +
-  theme(legend.position = "none")
-test_plot_2
+#x_value = "affiliation_shortened"
+##df.unpackt <- datasets.df %>% separate_longer_delim(!!sym(x_value), delim = ",")
+#df.unpackt <- bind_rows(datasets.df$entries) %>%
+  #unnest(!!sym(x_value))
+#df.unpackt[[x_value]]
+#dim(df.unpackt)
+#min_value = 0
+#order_bars <- FALSE
 
-#ggplot(datasets.df, aes(x= fields.publication_date)) + geom_bar() + theme_classic()
+#test_plot_2 <- ggplot(df.unpackt  %>% group_by(!!sym(x_value)) %>% filter(n() > min_value), aes( #!!sym() converts character to variable
+  #if(order_bars == TRUE) {x = fct_rev(fct_infreq(!!sym(x_value)))} 
+  #else {x = !!sym(x_value)}
+#))+ #input$variable is a string, !!sym() converts them into symbols
+  #geom_bar(aes(fill = as.factor(after_stat(count)))) + #fill-component in aes needs to be declared here, because it is not compatible with aes_string ##this is probably not necessary after all with the impl of !!sym(), but we will keep it to make it easier to read
+  #theme_classic() + #remove gridline +
+  #theme(axis.text.x = element_text(angle = -45)) +
+  #scale_fill_manual(values = pal) + #enables the custom palette from wes_palette
+  #theme(legend.position = "none") # No legend
+#test_plot_2
 
-x_value = "fields.tissue"
-y_value = "fields.species"
-heatmap_test <- ggplot(datasets.unpackt %>% group_by(!!sym(x_value)) %>% filter(n() > 0), 
-                       aes(y = !!sym(y_value), x = as.character(!!sym(x_value)), fill = as.factor(after_stat(count)))
-                       ) + 
-  geom_bin2d() +
-  theme_classic() +
-  scale_fill_manual(values = wes_palette("Zissou1", n = 8, type = "continuous"))
+##ggplot(datasets.df, aes(x= fields.publication_date)) + geom_bar() + theme_classic()
 
-heatmap_test
-
-heatmap.layer <- layer_data(heatmap_test, i = 1L)
-
-length(unique(heatmap.layer$count))
+#x_value = "fields.tissue"
+#y_value = "fields.species"
+#heatmap_test <- ggplot(datasets.unpackt %>% group_by(!!sym(x_value)) %>% filter(n() > 0), 
+                       #aes(y = !!sym(y_value), x = as.character(!!sym(x_value)), fill = as.factor(after_stat(count)))
+                       #) + 
+  #geom_bin2d() +
+  #theme_classic() +
+  #scale_fill_manual(values = wes_palette("Zissou1", n = 8, type = "continuous"))
 
 #-------------------
 
@@ -144,21 +138,7 @@ wordcloud <- ggplot(datasets.unpackt, aes(label = unique(fields.submitter_keywor
 #wordcloud
 #--------------------------
 
-input <- "affiliation_shortened"
-order_bars <- FALSE #we want institutions to be ordered after frequency, but not the years of publications
 
-if(input == "affiliation_shortened" |
-   input == "fields.species" |
-   input == "fields.disease"
-){order_bars <- TRUE}
-ggplot(datasets.df %>% group_by(!!sym(input)) %>% filter(n() > 2), aes( #!!sym() converts character to variable
-  if(order_bars == TRUE) {x = fct_rev(fct_infreq(!!sym(input)))} 
-  else {x = !!sym(input)}
-))+ #input$variable is a string, !!sym() converts them into symbols
-  geom_bar(aes(fill = as.factor(after_stat(count)))) + #fill-component in aes needs to be declared here, because it is not compatible with aes_string ##this is probably not necessary after all with the impl of !!sym(), but we will keep it to make it easier to read
-  theme_classic() + #remove gridlines
-  scale_fill_manual(values = pal) + #enables the custom palette from wes_palette
-  theme(legend.position = "none") # No legend
 
 ## Barplots
 # Define UI for EBI-db-app app ----
@@ -181,7 +161,12 @@ ui <- fluidPage(
                     "Publication date" = "fields.publication_date",
                     "Affiliation" = "affiliation_shortened",
                     "Species" = "fields.species",
-                    "Disease" = "fields.disease"
+                    "Disease" = "fields.disease",
+                    "Tissue" = "fields.tissue",
+                    "Instrument" = "fields.instrument_platform",
+                    "Modifications" = "fields.modification",
+                    "Keywords" = "fields.submitter_keywords",
+                    "Omics" = "fields.omics_type"
                     )),
       uiOutput("numeric")  #This renders the slider input for some plots
       
@@ -200,9 +185,10 @@ ui <- fluidPage(
   )
 )
 
+exceptions <- c("fields.publication_date", "fields.submission_date") #these bar_plots will be ordered in chronological order, not after count
 
 # Define server logic to plot various variables against count ----
-server <- function(input, output) {
+server <- function(input, output) { #shiny passes selectInput as a string. To use these variables for subsetting dataframes, use e.g. df$!!sym(input$variable) or df[[input$variable]]
   
   # Compute the formula text ----
   # This is in a reactive expression since it is shared by the
@@ -219,12 +205,11 @@ server <- function(input, output) {
   # Render slider input for certain plots
   
   output$numeric <- renderUI({
-    if(input$variable == "affiliation_shortened"|
-       input$variable == "fields.species"|
-       input$variable == "fields.disease") {
+    if(!(input$variable %in% exceptions)) { #if the input is NOT among the exceptions, render the min+max input options
+      #find.count(datasets.df)
       list( # for the inputs below to be succesfully rendered inside an if-statement, we need to wrap them inside a list
-      numericInput(inputId = "min","Exclude entries with counts lower than", 0),
-      numericInput(inputId = "max","Exclude entries with counts higher than", 0)
+      numericInput(inputId = "min","Exclude entries with counts lower than", 1),
+      numericInput(inputId = "max","Exclude entries with counts higher than", 1)
       )
     }
   }
@@ -233,24 +218,41 @@ server <- function(input, output) {
 
   # Generate a plot of the requested variable against count ----
 
-  output$ebi.plot <- renderPlot({
-    if(input$variable == "affiliation_shortened" |
-       input$variable == "fields.species" |
-       input$variable == "fields.disease"
-    ){order_bars <- TRUE
-      min_value <- input$min}
+  output$ebi.plot <- renderPlot({ #The fields below are sorted chronologically, not after count5
+    #counts <- find.count(datasets.df, input$variable)
+    #print(counts)
+    if(input$variable %in% exceptions
+    ){order_bars <- FALSE
+      min_value <- 1
+      }
     else{
-      order_bars <- FALSE
-      min_value <- 1 
+      order_bars <- TRUE
+      min_value <- input$min 
     }
-    ggplot(datasets.df %>% group_by(!!sym(input$variable)) %>% filter(n() > min_value), aes( #!!sym() converts character to variable
-      if(order_bars == TRUE) {x = fct_rev(fct_infreq(!!sym(input$variable)))} 
-      else {x = !!sym(input$variable)}
+    #datasets.tibble <- as.tibble(datasets.df)
+    if(class(datasets.df[[input$variable]])=="list"){
+      df.unpackt <- (unpack <- datasets.df %>%
+                       rowwise() |>
+                       mutate(
+                         fields.species =
+                           list(as.character(!!sym(input$variable)))) |>
+                       unnest_longer(!!sym(input$variable)))
+      plot.df <- df.unpackt
+    }
+    else{
+      plot.df <- datasets.df
+    }
+    ggplot(plot.df  %>% group_by(!!sym(input$variable))
+           %>% filter(n() > min_value),
+           aes( fill = affiliation_shortened, #!!sym() converts character to variable
+      if(order_bars == TRUE) {x = fct_rev(fct_infreq(!!sym(input$variable)))} #sort bars after count, lowest to highest
+      else {x = !!sym(input$variable)} #sort chronologically
       ))+ #input$variable is a string, !!sym() converts them into symbols
-      geom_bar(aes(fill = as.factor(after_stat(count)))) + #fill-component in aes needs to be declared here, because it is not compatible with aes_string ##this is probably not necessary after all with the impl of !!sym(), but we will keep it to make it easier to read
-      theme_classic() + #remove gridlines
-      scale_fill_manual(values = pal) + #enables the custom palette from wes_palette
-      theme(legend.position = "none") # No legend
+      geom_bar(position = "stack") + #fill-component in aes needs to be declared here, because it is not compatible with aes_string ##this is probably not necessary after all with the impl of !!sym(), but we will keep it to make it easier to read
+      theme_classic() + #remove gridline
+      theme(axis.text.x = element_text(angle = -45)) +
+      scale_fill_brewer(palette = "Paired")
+      #theme(legend.position = "none") # No legend
   })
   
 }
